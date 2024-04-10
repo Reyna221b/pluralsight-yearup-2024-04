aee6132822ec778b7b206ab30530604e388982c7
Reyna <Reyna221b@outlook.com>
2024-04-10 15:59:51 -0700

    declaring varibales exercise/notes

--- workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
+++ workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
@@ -7,34 +7,42 @@ public class Main
         // declare variables for each of the following:
 
         // a vehicle identification number in the range 1000000 - 9999999
+        int vehicleIDNum = 1234567;
 
 
         // a vehicle make /model (i.e. Ford Explorer)
+        String carModel = "Ford Explorer";
 
 
         // a vehicle color
+        String vehicleColor = "red";
+
 
 
         // whether the vehicle has a towing package
+        boolean includesTowingPkg = true;
 
 
         // an odometer reading
-
+        byte odometerReading = 123;
 
         // a price
+        float carPrice = 10000.23f;
 
 
         // a quality rating (A, B, or C)
+        char qualityRating = 'a';
 
 
         // a phone number
-
+        String phoneNum = "1112223333";
 
         // a social security number
+        String socialSN = "1234567891";
 
 
         // a zip code
-
+        int zipCode = 94522;
 
 
     }
--- workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
+++ workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
@@ -4,6 +4,19 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        //variable naming
+        //mustn
+        byte count;
+        count =127;
+        byte extraStudent = 1;
+
+        count = (byte)(count + 1);
+        System.out.println(count);
+
+        //are all the kids on the bus
+        boolean kidsOnBus = true;
+
+        //the first letter of the alphabet
+        char firstLetter = 'a';
     }
 }
