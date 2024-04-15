6569c337aa98c48d7d8e8371bb6966f46c54d44c
Reyna <Reyna221b@outlook.com>
2024-04-15 15:01:12 -0700

    FullNameGenerator & FullNameParser

---
+++ workbook-1/FullNameGenerator/src/main/java/org/pluralsight/Main.java
@@ -0,0 +1,52 @@
+package org.pluralsight;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static Scanner userInput = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+
+
+        String firstName;
+        String middleName;
+        String lastName;
+        String suffix;
+        String nameFormat;
+
+        System.out.println("Please enter your name: \n");
+        System.out.print("First name: ");
+        firstName = userInput.nextLine().trim();
+
+        System.out.print("Middle name: ");
+        middleName = userInput.nextLine().strip();
+
+        System.out.print("Last name: ");
+        lastName = userInput.nextLine().trim();
+
+        System.out.print("Suffix: ");
+        suffix = userInput.nextLine().trim();
+
+        formattedName(firstName,middleName,lastName,suffix);
+
+    }
+
+    public static void formattedName(String first, String middle, String last, String suf)
+    {
+
+        if (!middle.isEmpty()) {
+            if(!suf.isEmpty()) {
+                System.out.printf("\nFull name: %s %s. %s, %s", first, middle, last, suf);
+                return;
+            }
+            System.out.printf("\nFull name: %s %s. %s", first, middle, last);
+            return;
+        } else if (!suf.isEmpty()) {
+            System.out.printf("\nFull name: %s %s, %s", first, last, suf);
+            return;
+        }
+
+        System.out.printf("Full name: %s %s", first, last);
+    }
+}
---
+++ workbook-1/FullNameParser/src/main/java/org/pluralsight/Main.java
@@ -0,0 +1,39 @@
+package org.pluralsight;
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Scanner userInput = new Scanner(System.in);
+        String nameInput;
+        String firstName;
+        String middleName;
+        String lastName;
+
+        System.out.print("Please enter your full name: ");
+        nameInput = userInput.nextLine().strip();
+        String[] fullName = nameInput.split(" ");
+
+
+
+
+        if (fullName.length > 2){
+            firstName = fullName[0];
+            middleName = fullName[1];
+            lastName= fullName[2];
+
+            System.out.printf("First name: %s\nMiddle name: %s\nLast name: %s",firstName, middleName, lastName);
+
+        }
+        else {
+            firstName = fullName[0];
+            lastName= fullName[1];
+
+            System.out.printf("First name: %s\nLast name: %s\n", firstName, lastName);
+
+        }
+    }
+}
