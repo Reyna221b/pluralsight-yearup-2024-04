49dffeb510e73fc033477a09d990e2aee6d93f63
Reyna <Reyna221b@outlook.com>
2024-04-17 11:10:56 -0700

    CellPhoneService exercises starter

---
+++ workbook-2/CellPhoneService/src/main/java/org/pluralsight/CellPhone.java
@@ -0,0 +1,12 @@
+package org.pluralsight;
+
+public class CellPhone
+{
+    private double serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String Owner;
+
+    
+}
