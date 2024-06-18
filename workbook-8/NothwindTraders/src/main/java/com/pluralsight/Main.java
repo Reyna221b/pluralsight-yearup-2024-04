530664e2c45d42e5d6110a9ac92d77dbf28bed81
Reyna <Reyna221b@outlook.com>
2024-06-18 15:25:27 -0700

    Ex. 1

--- workbook-8/NothwindTraders/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NothwindTraders/src/main/java/com/pluralsight/Main.java
@@ -14,98 +14,49 @@ public class Main
         int option = display(scanner);
         String username = "root";
         String password = "Pokemon1.";
-
-        // load the MySQL Driver
+        Connection connection = null;
 
         try {
+            // load the MySQL Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
+
             // 1. open a connection to the database
-            // use the database URL to point to the correct database
-            Connection connection;
             connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/northwind",
                     username,
                     password);
 
-
-
-            // 2. execute a statement
-            String sql;
-            PreparedStatement statement = null;
-            ResultSet resultSet = null;
+            switch (option) {
+                case 1:
+                    displayProducts(connection);
+                    break;
+                case 2:
+                    displayCustomers(connection);
+                    break;
+                case 3:
+                    displayCategories(connection, scanner);
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error loading MySQL driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database: " + e.getMessage());
+        } finally {
             try {
-                switch (option) {
-                    case 1:
-                        sql = """
-                        SELECT ProductID
-                            , ProductName
-                            , UnitPrice
-                            , UnitsInStock
-                        FROM products;
-                        """;
-                        statement = connection.prepareStatement(sql);
-                        resultSet = statement.executeQuery(sql);
-
-                        System.out.print("Product ");
-                        while (resultSet.next()) {
-                            int id = resultSet.getInt("ProductID");
-                            String name = resultSet.getString("ProductName");
-                            float unitPrice = resultSet.getFloat("UnitPrice");
-                            int unitStock = resultSet.getInt("UnitsInStock");
-
-                            System.out.printf(" ID: %3d\n Name: %-32s\n Price: %.2f\n Stock: %d\n", id, name,unitPrice,unitStock);
-                            System.out.println("-".repeat(37));
-                        }
-                        break;
-                    case 2:
-                        sql = """
-                        SELECT  CompanyName
-                            , ContactName
-                            , city
-                            , Country
-                            , phone
-                        FROM customers
-                        ORDER BY Country;
-                        """;
-                        statement = connection.prepareStatement(sql);
-                        resultSet = statement.executeQuery(sql);
-
-
-                        while (resultSet.next()) {
-
-                            String company = resultSet.getString("CompanyName");
-                            String contact = resultSet.getString("ContactName");
-                            String country = resultSet.getString("Country");
-                            String city = resultSet.getString("City");
-                            String phone = resultSet.getString("Phone");
-
-                            System.out.printf("Company: %-10s\n ContactName: %-32s\n City: %-20s\n Country: %-20s\n Phone: %-20s\n",company, contact, city, country, phone);
-                            System.out.println("-".repeat(52));
-                        }
-                        break;
-                    case 0:
-                        System.out.println("Exiting...");
-                        break;
-                    default:
-                        System.out.println("Invalid option");
-                        break;
+                if (connection != null) {
+                    connection.close();
                 }
             } catch (SQLException e) {
-                System.out.println("Error executing query: " + e.getMessage());
-            } finally {
-                // close the connection, statement, and resultSet
-                try {
-                    if (resultSet != null) resultSet.close();
-                    if (statement != null) statement.close();
-                    if (connection != null) connection.close();
-                } catch (SQLException e) {
-                    System.out.println("Error closing resources: " + e.getMessage());
-                }
+                System.out.println("Error closing connection: " + e.getMessage());
             }
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Error loading MySQL driver: " + e.getMessage());
         }
-        
+
     }
 
     public static int display(Scanner scanner) {
@@ -119,7 +70,7 @@ public class Main
                 System.out.println("[0] Exit");
                 System.out.print("Select an option: ");
                 option = scanner.nextInt();
-                if (option < 0 || option > 2) {
+                if (option < 0 || option > 3) {
                     System.out.println("Invalid option. Please select a valid option.");
                     continue;
                 }
@@ -131,5 +82,118 @@ public class Main
         }
         return option;
     }
+
+    public static void displayProducts(Connection connection) {
+        String sql = """
+        SELECT ProductID
+            , ProductName
+            , UnitPrice
+            , UnitsInStock
+        FROM products;
+        """;
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            System.out.print("Product ");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ProductID");
+                String name = resultSet.getString("ProductName");
+                float unitPrice = resultSet.getFloat("UnitPrice");
+                int unitStock = resultSet.getInt("UnitsInStock");
+
+                System.out.printf(" ID: %3d\n Name: %-32s\n Price: %.2f\n Stock: %d\n", id, name, unitPrice, unitStock);
+                System.out.println("-".repeat(37));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error displaying products: " + e.getMessage());
+        }
+    }
+
+    public static void displayCustomers(Connection connection) {
+        String sql = """
+        SELECT CompanyName
+            , ContactName
+            , City
+            , Country
+            , Phone
+        FROM customers
+        ORDER BY Country;
+        """;
+        try (PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            while (resultSet.next()) {
+
+                String company = resultSet.getString("CompanyName");
+                String contact = resultSet.getString("ContactName");
+                String city = resultSet.getString("city");
+                String country = resultSet.getString("Country");
+                String phone = resultSet.getString("Phone");
+
+                System.out.printf(" Company: %-10s\n Contact: %-32s\n City: %-20s\n Country: %-20s\n Phone: %-20s\n", company, contact,city, country, phone);
+                System.out.println("-".repeat(52));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error displaying customers: " + e.getMessage());
+        }
+    }
+
+    public static void displayCategories(Connection connection, Scanner scanner) {
+        String sql = """
+        SELECT CategoryID
+            , CategoryName
+            , Description
+        FROM categories
+        ORDER BY CategoryID;
+        """;
+        try ( PreparedStatement statement = connection.prepareStatement(sql);
+              ResultSet resultSet = statement.executeQuery(sql))
+        {
+
+            System.out.print("Category ");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("CategoryID");
+                String name = resultSet.getString("CategoryName");
+                String desc = resultSet.getString("Description");
+
+                System.out.printf(" ID: %3d\n Name: %-32s\n Description: %-50s\n", id, name, desc);
+                System.out.println("-".repeat(37));
+            }
+
+            System.out.print("Enter a Category ID to display products: ");
+            int categoryID = scanner.nextInt();
+
+            sql = """
+            SELECT ProductID
+                , ProductName
+                , UnitPrice
+                , UnitsInStock
+            FROM products
+            WHERE CategoryID = ?
+            ORDER BY ProductID;
+            """;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, categoryID);
+            ResultSet result = preparedStatement.executeQuery();
+
+            System.out.print("Product ");
+            while (result.next())
+            {
+                int id = result.getInt("ProductID");
+                String name = result.getString("ProductName");
+                float unitPrice = result.getFloat("UnitPrice");
+                int unitStock = result.getInt("UnitsInStock");
+
+                System.out.printf(" ID: %3d\n Name: %-32s\n Price: %.2f\n Stock: %d\n", id, name, unitPrice, unitStock);
+                System.out.println("-".repeat(37));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error displaying categories: " + e.getMessage());
+        }
+    }
+
+
+
+
 
 }
